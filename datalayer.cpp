17d10dcb881585f1a3953ccb09e0c451d1f45ecc
brietk <brietkonn@gmail.com>
2015-11-30 19:30:53 +0000

    Datalayer frá Búa

--- datalayer.cpp
+++ datalayer.cpp
@@ -2,71 +2,82 @@
 #include "person.h"
 #include "datalayer.h"
 #include <cstdlib>
+#include <iostream>
+
+using namespace std;
 
 DataLayer::DataLayer()
 {
-    createTestData();
-}
 
-void DataLayer::SaveData(Person p)
-{
-    /*ofstream out_stream;
-    out_stream.open("data.txt", ios::app);
-    if(out_stream.fail())
-    {
-        cout << "in stream fail";
-        exit(1);
-    }
-    out_stream << p.getName() << endl;
-    out_stream << p.getGender() << endl;
-    out_stream << p.getDayOfBirth() << endl;
-    out_stream << p.getDayOfDeath() << endl;
-    out_stream.close();*/
-    m_personList.push_back(p); //á ekki að vera
 }
 
 vector<Person> DataLayer::GetData()
 {
-    /*ifstream in_stream;
+    ifstream in_stream;
     in_stream.open("data.txt");
-    if(in_stream.fail())
+
+    m_personList.clear();
+    if(in_stream.is_open())
     {
-        cout << "in stream fail";
-        exit(1);
+
+        string name;
+        string gender;
+        int dob;
+        int dod;
+
+        while (!in_stream.eof())
+        {
+            in_stream >> name;
+            in_stream >> gender;
+            in_stream >> dob;
+            in_stream >> dod;
+
+            Person newguy(name, gender, dob, dod);
+            m_personList.push_back(newguy);
+        }
+
+        in_stream.close();
     }
-    string name;
-    string gender;
-    int dob;
-    int dod;
-
-    while (!in_stream.eof())
-    {
-        in_stream >> name;
-        in_stream >> gender;
-        in_stream >> dob;
-        in_stream >> dod;
-
-        Person newguy(name, gender, dob, dod);
-        m_personList.push_back(newguy);
-    }
-
-    in_stream.close();*/
     return m_personList;  //Búi breytir þessu kannski? Ef það er
 }                         //eðlilegra að sækja gögnin í skrá í staðin
                           //fyrir að sækja þau úr vector.
+
+void DataLayer::SaveData(vector<Person>& m_personList)
+{
+    ofstream out_stream;
+    out_stream.open("data.txt");
+
+    for(unsigned int i=0; i<m_personList.size(); i++)
+    {
+        out_stream << m_personList[i].getName() << endl;
+        out_stream << m_personList[i].getGender() << endl;
+        out_stream << m_personList[i].getDayOfBirth() << endl;
+        out_stream << m_personList[i].getDayOfDeath() << endl;
+    }
+    out_stream.close();
+}
+
+void DataLayer::SaveData(Person p)
+{
+    ofstream out_stream;
+    out_stream.open("data.txt");
+
+    out_stream << p.getName() << endl;
+    out_stream << p.getGender() << endl;
+    out_stream << p.getDayOfBirth() << endl;
+    out_stream << p.getDayOfDeath() << endl;
+
+    out_stream.close();
+}
+
+void DataLayer::AddData(Person& p)
+{
+    m_personList.push_back(p);
+}
 
 void swap(Person& p1, Person& p2)
 {
     Person temp = p1;
     p1 = p2;
     p2 = temp;
-}
-
-void DataLayer::createTestData()   // temp. prufu data.
-{
-    Person computerduddi("Alan Turing", "m", 1936, 1989);
-    Person computerduddi2("Britta konn", "m", 1936, -1);
-    SaveData(computerduddi);
-    SaveData(computerduddi2);
-    swap(m_personList[0], m_personList[1]);
 }
--- datalayer.h
+++ datalayer.h
@@ -2,7 +2,6 @@
 #define DATALAYER_H
 #include "person.h"
 #include <vector>
-#include <fstream>
 
 
 class DataLayer
@@ -10,7 +9,10 @@ class DataLayer
 public:
     DataLayer();
     vector<Person> GetData(); // skilar öllum skráðum persónum.
-    void SaveData(Person p);  // vistar eina persónu.
+    void SaveData(vector<Person>& m_personList);  // vistar vector af persónum.
+    void AddData(Person& p); // bætir persónu við
+    void SaveData(Person p); // vistar eina persónu
+
 
 private:
     vector<Person> m_personList; // geymir lista af persónum.
